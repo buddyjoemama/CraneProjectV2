d74eaf31e006c4d4831f30a654ce73f733d44da3
Brian Bacon <brian.bacon01@gmail.com>
2019-12-02 22:00:20 -0500

    updates

---
+++ Server/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_from_directory, Response
+#from controls import NorthChip, SouthChip
+import SerialController
+
+app = Flask(__name__)
+
+sController = SerialController.SerialController('/dev/ttyACM0')
+
+@app.route('/py/control/north/<int:nOp>/south/<int:sOp>')
+def control(nOp, sOp):
+    #north = NorthChip(nOp)
+    #south = SouthChip(sOp)    
+    
+    #sController.write(1 << north, 1 << south)
+    return Response("Ok", status=200)
+
+if __name__ == '__main__':
+    app.run(debug=True)
